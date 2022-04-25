d4aefa05c414a2fcd57571a05abc27f960a8767b
Aenoris <aenoriss@gmail.com>
2022-04-25 07:54:08 -0300

    asad

--- models/trigger.js
+++ models/trigger.js
@@ -17,9 +17,6 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Trigger.init({
-    egg: {
-      type: DataTypes.INTEGER
-    },
     taken: {
       type: DataTypes.BOOLEAN,
       defaultValue: false,
